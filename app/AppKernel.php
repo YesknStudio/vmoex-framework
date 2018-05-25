ae62c2ddac9dc23c18eae9b5345669591bffe382
Jaggle <singviy@gmail.com>
2018-05-25 14:36:05 +0800

    refactor

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,7 +15,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
             new Yeskn\BlogBundle\YesknBlogBundle(),
             new Yeskn\AdminBundle\YesknAdminBundle(),
             new Yeskn\CommonBundle\YesknCommonBundle(),
