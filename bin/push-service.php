571b6f83f37b320635f8f69a8d5db3f40743a4e1
Jaggle <singviy@gmail.com>
2018-06-11 00:30:06 +0800

    change i18n format

--- bin/push-service.php
+++ bin/push-service.php
@@ -13,8 +13,17 @@ $last_online_count = 0;
 // 记录最后一次广播的在线页面数
 $last_online_page_count = 0;
 
+// ssl context
+$context = array(
+    'ssl' => array(
+        'local_cert'  => getenv('CERT_PATH'),
+        'local_pk'    => getenv('KEY_PATH'),
+        'verify_peer' => false,
+    )
+);
+
 // PHPSocketIO服务
-$sender_io = new SocketIO(2120);
+$sender_io = new SocketIO(2120, $context);
 // 客户端发起连接事件时，设置连接socket的各种事件回调
 $sender_io->on('connection', function(\PHPSocketIO\Socket $socket){
     // 当客户端发来登录事件时触发
