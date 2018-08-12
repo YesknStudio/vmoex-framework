9c0a0a0850b2767a8191d15884dfdd98d4bd026f
Jaggle <singviy@gmail.com>
2018-08-12 11:47:40 +0800

    update admin home page

--- src/Yeskn/AdminBundle/Controller/DefaultController.php
+++ src/Yeskn/AdminBundle/Controller/DefaultController.php
@@ -11,12 +11,27 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
  */
 class DefaultController extends Controller
 {
+
     /**
      * @Route("/admin")
      * @return \Symfony\Component\HttpFoundation\Response
      */
     public function indexAction()
     {
-        return $this->render('YesknAdminBundle:Default:index.html.twig');
+        $postCount = $this->getDoctrine()->getRepository('YesknBlogBundle:Post')->countPost();
+        $userCount = $this->getDoctrine()->getRepository('YesknBlogBundle:User')->countUser();
+        $commentCount = $this->getDoctrine()->getRepository('YesknBlogBundle:Comment')->countComment();
+
+        $todayLoginUserCount = $this->getDoctrine()->getRepository('YesknBlogBundle:User')
+            ->getTodayLoggedUserCount();
+
+        return $this->render('YesknAdminBundle:Default:index.html.twig', [
+            'count' => [
+                'post' => $postCount,
+                'user' => $userCount,
+                'todayLoginUserCount' => $todayLoginUserCount,
+                'comment' => $commentCount
+            ]
+        ]);
     }
 }
--- src/Yeskn/AdminBundle/Controller/PostController.php
+++ src/Yeskn/AdminBundle/Controller/PostController.php
@@ -5,6 +5,7 @@ namespace Yeskn\AdminBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Yeskn\BlogBundle\Entity\Post;
@@ -15,7 +16,7 @@ use Yeskn\BlogBundle\Entity\Tag;
  * @Route("/admin/post")
  * @Security("has_role('ROLE_ADMIN')")
  */
-class PostController extends AdminCommonController
+class PostController extends Controller
 {
     /**
      * @Route("/create")
