164f53fda9ee7a185456bc60724529d31901929f
Jaggle <singviy@gmail.com>
2018-10-01 19:46:58 +0800

    markdown support

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new Knp\Bundle\MarkdownBundle\KnpMarkdownBundle(),
             new Yeskn\MainBundle\YesknMainBundle(),
             new Yeskn\AdminBundle\YesknAdminBundle(),
             new Yeskn\OAuthBundle\YesknOAuthBundle(),
