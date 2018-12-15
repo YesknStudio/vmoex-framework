c1464f3d6481aeebff5ce8077856ff73ebdd3493
Jaggle <singviy@gmail.com>
2018-12-15 19:22:15 +0800

    fix travis

---
+++ src/Yeskn/MainBundle/Tests/Framework/ParameterConfigTest.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * This file is part of project vmoex-framework.
+ *
+ * Author: Jake
+ * Create: 2018-12-15 19:09:21
+ */
+
+namespace Yeskn\MainBundle\Tests\Framework;
+
+use Symfony\Bundle\FrameworkBundle\Tests\TestCase;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+class ParameterConfigTest extends KernelTestCase
+{
+    /** @var KernelInterface */
+    private $realKernel;
+
+    protected function setUp()
+    {
+        $this->realKernel = static::bootKernel([]);
+    }
+
+    public function testSecret()
+    {
+
+        $container = $this->realKernel->getContainer();
+
+        $this->assertEquals('e01b91e2f072deac5abf039b4f6e6d9149442399', $container->getParameter('secret'));
+    }
+}
