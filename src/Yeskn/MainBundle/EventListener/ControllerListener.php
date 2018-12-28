7553dfd0a47e88c63b91635f83f6e7e79f1d4eca
Jaggle <singviy@gmail.com>
2018-12-28 13:10:59 +0800

    fix controller listener

--- src/Yeskn/MainBundle/EventListener/ControllerListener.php
+++ src/Yeskn/MainBundle/EventListener/ControllerListener.php
@@ -1,6 +1,7 @@
 <?php
+
 /**
- * This file is part of project JetBlog.
+ * This file is part of project yeskn-studio/vmoex-framework.
  *
  * Author: Jake
  * Create: 2018-05-27 15:38:34
@@ -12,22 +13,22 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Yeskn\MainBundle\Entity\User;
-use Yeskn\MainBundle\Repository\ActiveRepository;
 
 class ControllerListener
 {
     static $increasedTodayActive = false;
+
     private $tokenStorage;
 
     /**
-     * @var ActiveRepository
+     * @var EntityManagerInterface
      */
-    private $repository;
+    private $em;
 
     public function __construct(TokenStorageInterface $tokenStorage, EntityManagerInterface $em)
     {
         $this->tokenStorage = $tokenStorage;
-        $this->repository = $em->getRepository('YesknMainBundle:Active');
+        $this->em = $em;
     }
 
     public function onKernelController()
@@ -37,7 +38,7 @@ class ControllerListener
 
         if ($user && self::$increasedTodayActive == false) {
             self::$increasedTodayActive = true;
-            $this->repository->increaseTodayActive($user);
+            $this->em->getRepository('YesknMainBundle:Active')->increaseTodayActive($user);
         }
     }
 
