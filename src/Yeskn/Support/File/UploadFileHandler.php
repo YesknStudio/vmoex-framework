47ef03af9d79b7335487a23b5a1ce5328436757b
Jaggle <singviy@gmail.com>
2019-04-19 22:31:57 +0800

    fix file upload

--- src/Yeskn/Support/File/UploadFileHandler.php
+++ src/Yeskn/Support/File/UploadFileHandler.php
@@ -11,6 +11,7 @@ namespace Yeskn\Support\File;
 
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpFoundation\File\File;
+use Yeskn\Support\ParameterBag;
 
 class UploadFileHandler
 {
@@ -18,22 +19,14 @@ class UploadFileHandler
 
     protected $fileName;
 
-    public function __construct($projectDir)
+    /**
+     * @param File $file
+     * @throws \Symfony\Component\Filesystem\Exception\FileNotFoundException
+     * @throws \Symfony\Component\Filesystem\Exception\IOException
+     * @return string
+     */
+    private function save($file)
     {
-        $this->basePath = $projectDir . '/web';
-    }
-
-    public function upload($entity, $attribute)
-    {
-        $getMethod = 'get' . ucfirst($attribute);
-
-        if (!method_exists($entity, $getMethod)) {
-            throw new \Exception();
-        }
-
-        /** @var File $file */
-        $file = $entity->$getMethod();
-
         if (empty($this->fileName)) {
             $extension = $file->guessExtension();
             $this->fileName = '/upload/' . time() . mt_rand(1000, 9999) . '.' . $extension;
@@ -48,6 +41,31 @@ class UploadFileHandler
         $fs = new Filesystem();
         $fs->copy($file->getRealPath(), $targetPath);
 
+        return $targetPath;
+    }
+
+    public function __construct($projectDir)
+    {
+        $this->basePath = $projectDir . '/web';
+    }
+
+    public function upload($entity, $attribute)
+    {
+        if ($entity instanceof ParameterBag) {
+            return $this->uploadToParameterBag($entity, $attribute);
+        }
+
+        $getMethod = 'get' . ucfirst($attribute);
+
+        if (!method_exists($entity, $getMethod)) {
+            throw new \Exception();
+        }
+
+        /** @var File $file */
+        $file = $entity->$getMethod();
+
+        $targetPath = $this->save($file);
+
         $method = 'set' . ucfirst($attribute);
 
         if (method_exists($entity, $method)) {
@@ -57,5 +75,21 @@ class UploadFileHandler
         }
 
         throw new \Exception();
+    }
+
+    /**
+     * @param ParameterBag $entity
+     * @param $attribute
+     * @return UploadedFile
+     */
+    public function uploadToParameterBag($entity, $attribute)
+    {
+        $file = $entity->get($attribute);
+
+        $target = $this->save($file);
+
+        $entity->set($attribute, $this->fileName);
+
+        return new UploadedFile($target);
     }
 }
